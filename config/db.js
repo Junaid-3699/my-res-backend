ad233354c2280de137a4c45e875719151cdbbd75
Junaid Mehroz <junaidmehroz3@gmail.com>
2023-07-23 21:25:01 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const connectToDB = async() => {
     try {
-       const conn =  await mongoose.connect(process.env.MONGO_URL)
+       const conn =  await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser : true,
+        useUnifiedTopology : true
+   })
        console.log(`MngoDB connected ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
